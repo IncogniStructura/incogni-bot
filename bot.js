4f5c46c57c181bec96bf1839fd27f3cf6e2fcf58
IncogniStructura <benerianstudios@gmail.com>
2018-04-07 18:58:45 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,30 +13,30 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', function(message) {
+client.on('message', msg => {
   //Process assets
   assets.recentmessage = msg.content;
   assets.processed = assets.recentmessage.toLowerCase();
 
-  if (assets.processed.indexOf("~") === -1) {
+  if (assets.processed.indexOf("$") === -1) {
   if (assets.processed.indexOf("ping") != -1) {
     if (assets.processed.indexOf("ping pong") != -1) {
-      client.sendMessage(message.channel,"~ I can't play that game.");
+      msg.reply("$ I can't play that game.");
     } else {
-      client.sendMessage(message.channel,"~ Pong Check!");
+      msg.reply("$ Pong Check!");
     }
   }
   //Stop
 	  if (assets.processed.indexOf("stop") != -1) {
 	    if (assets.processed.indexOf("stop now") != -1) {
-	      client.sendMessage(message.channel,"Alright, stopping now.");
+	      msg.reply("$ Alright, stopping now.");
             } else {
-	      client.sendMessage(message.channel,"Are you adressing me?");
+	      msg.reply("$ Are you adressing me?");
 	    }
 	  }
   //Start
   	  if (assets.processed.indexOf("you") != -1 && assets.processed.indexOf("there") != -1) {
-  	    client.sendMessage(message.channel,"Yeah, I'm here.");
+  	    msg.reply("$ Yeah, I'm here.");
   	  }
 	  
   }
