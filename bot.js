3db94b201a2ad1126de738e43e4303d1e060457a
IncogniStructura <benerianstudios@gmail.com>
2018-04-07 18:22:38 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,29 +4,14 @@ var settings = {
 	muted: false
 };
 
-client.on("ready", function () {
-	console.log("Ready to begin! Serving in " + client.channels.length + " channels");
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// This code will run once the client has disconnected from Discord.
-client.on("disconnected", function () {
-	// alert the console
-	console.log("Disconnected!");
-
-	// exit node.js with an error
-	process.exit(1);
-});
-
-// This code will run once the client receives any message.
-client.on("message", function (msg) {
-	// if message begins with "ping"
-	if (msg.content.indexOf("ping") === 0) {
-		// send a message to the channel the ping message was sent in.
-		client.sendMessage(msg.channel, "pong!");
-
-		// alert the console
-		console.log("pong-ed " + msg.author.username);
-	}
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
