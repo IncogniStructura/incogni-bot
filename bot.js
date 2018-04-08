66f14117389446258a081555b006688b083b1158
IncogniStructura <benerianstudios@gmail.com>
2018-04-07 19:11:30 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,20 @@ var settings = {
 var assets = {
   recentmessage: "",
   processed: "",
-  wordbankstop: []
+};
+var banks = {
+  startconvomode: 0,
+  start: ["~ Yeah, I'm here.","~ Hallo.","~ Hello.","~ What? Oh sorry 'bout that.","~ Was programming.","~ Are you there?"],
+  yespositive: ["~ Whew.", "~ That's nice to know.", "~ Good.", "~ Welp.", "~ Well."]
+};
+var $i = {
+	getbank: function (id) {
+	  var localrandom = Math.floor(Math.random() * id.length)
+	  if (id === "start" && localrandom === 6) {
+	    banks.startconvomode = 6;
+	  }
+	  return banks.id[localrandom];
+	}
 };
 
 client.on('ready', () => {
@@ -18,25 +31,32 @@ client.on('message', msg => {
   assets.recentmessage = msg.content;
   assets.processed = assets.recentmessage.toLowerCase();
 
-  if (assets.processed.indexOf("$") === -1) {
+  if (assets.processed.indexOf("~") === -1) {
   if (assets.processed.indexOf("ping") != -1) {
     if (assets.processed.indexOf("ping pong") != -1) {
-      msg.reply("$ I can't play that game.");
+      msg.reply("~ I can't play that game.");
     } else {
-      msg.reply("$ Pong Check!");
+      msg.reply("~ Pong Check!");
     }
   }
   //Stop
 	  if (assets.processed.indexOf("stop") != -1) {
 	    if (assets.processed.indexOf("stop now") != -1) {
-	      msg.reply("$ Alright, stopping now.");
+	      msg.reply("~ Alright, stopping now.");
             } else {
-	      msg.reply("$ Are you adressing me?");
+	      msg.reply("~ Are you adressing me?");
 	    }
 	  }
   //Start
   	  if (assets.processed.indexOf("you") != -1 && assets.processed.indexOf("there") != -1) {
-  	    msg.reply("$ Yeah, I'm here.");
+  	    msg.reply($i.getbank(start));
+  	  }
+  //Yes requests
+  	  if (assets.processed.indexOf("sure") != -1 || assets.processed.indexOf("yes") != -1 || assets.processed.indexOf("ye") != -1 || assets.processed.indexOf("ja") != -1) { 
+  	    if (banks.startconvomode === 6) {
+  	      msg.reply("~ Ah, good.");
+  	      banks.startconvomode = 0;
+  	    }
   	  }
 	  
   }
